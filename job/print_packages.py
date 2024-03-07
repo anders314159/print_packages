efb86438cee2bdb60563041ca18f4f3189e13a47
Anders Mundeling <andmde@erst.dk>
2024-03-07 10:30:31 +0100

    update

--- job/print_packages.py
+++ job/print_packages.py
@@ -1,3 +1,5 @@
+from os import path
+import sys
 import pkg_resources
 
 
@@ -6,7 +8,11 @@ class PrintsPackages:
         packages = sorted(["%s==%s" % (i.key, i.version) for i in pkg_resources.working_set])
         for package in packages:
             print(package)
-        return packages
+        py_ver = sys.version
+        print(py_ver)
+        where_interpreter = path.dirname(sys. executable) # where banana
+        print(where_interpreter)
+        return packages, py_ver, where_interpreter
 
 if __name__ == '__main__':
     pp = PrintsPackages()
